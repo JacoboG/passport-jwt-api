f59ca0020333ab57a0b93593596c1d3dbd1c9502
Omar Jacobo García <omar_192222@live.com>
2022-07-08 20:54:44 -0500

    Update authentication.js
    Remove Examples

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -4,11 +4,11 @@ const AuthenticationController = require('./../controllers/AuthenticateControlle
 const { ensureAuthenticated, ensureRefreshToken, ensureSuperadmin } = require('./../middlewares/AuthenticateMiddleware');
 const { registerValidator, loginValidator } = require('../middlewares/validators/AuthValidator');
 
-router.get('/', AuthenticationController.index);
+// router.get('/', AuthenticationController.index);
 router.post('/register', registerValidator, AuthenticationController.register);
 router.post('/login', loginValidator, AuthenticationController.login);
 router.post('/refresh-token', ensureRefreshToken, AuthenticationController.refreshToken);
 router.post('/logout', ensureAuthenticated, AuthenticationController.logout);
-router.post('/users', ensureAuthenticated, AuthenticationController.users);
+// router.post('/users', ensureAuthenticated, AuthenticationController.users);
 
 module.exports = router;
