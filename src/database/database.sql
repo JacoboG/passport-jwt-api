b20ef9f0dad7fc9fc9b77b6684bf1f9278244461
Omar Jacobo García <omar_192222@live.com>
2023-11-21 17:33:06 -0600

    Update database.sql

--- src/database/database.sql
+++ src/database/database.sql
@@ -83,8 +83,9 @@ DELIMITER $$
 CREATE TRIGGER `USERS_DELETE_TOKEN_ON_DEACTIVATED`
 AFTER UPDATE ON `users`
 FOR EACH ROW
-    IF (OLD.id_status = 1 AND NEW.id_status = 2) OR
-        (OLD.id_status = 1 AND NEW.id_status = 3)
+    IF (OLD.id_status = 1 AND NEW.id_status = 2) OR (OLD.id_status = 1 AND NEW.id_status = 3) 
+        OR (OLD.id_status = 2 AND NEW.id_status = 3)
+        OR (OLD.id_status = 3 AND NEW.id_status = 2)
     THEN 
         DELETE FROM tokens WHERE tokens.id_user = OLD.id_user;
     END IF
